b1bc6f2c630610f40aa16754ffd8e2c72eff20a0
Juan José Briasco <92890818+JJBandana@users.noreply.github.com>
2022-09-29 10:39:42 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-fold text-[#ccd6f6]'>Juan José Briasco</h1>
             <h2 className='text-4xl sm:text-7xl font-fold text-[#8892b0]'>I'm a Full Stack developer.</h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>Game developing is my passion since i am just a kid. Focused on every type of videogame.</p>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Programming is my passion since i am just a kid. Focused on every type of projects.</p>
             <div>
               <Link to='work' smooth={true} duration={1000}>
                 <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
